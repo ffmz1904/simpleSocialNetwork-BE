dc91dd4dee5334e599c436ab608f2051ef5caa0f
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-04-10 11:48:11 +0300

    update user model

--- controllers/userController/registration.js
+++ controllers/userController/registration.js
@@ -3,10 +3,10 @@ const User = require('../../models/User');
 const ApiError = require('../../errors/ApiError');
 
 module.exports = async (req, res, next) => {
-    const { email, password } = req.body;
+    const { email, password, name } = req.body;
 
-    if (!email || !password) {
-        return next(ApiError.badRequest('Undefined email or password!'));
+    if (!email || !password || !name) {
+        return next(ApiError.badRequest('Undefined email, name or password!'));
     }
 
     try {
@@ -17,7 +17,7 @@ module.exports = async (req, res, next) => {
         }
 
         const hashPassword = await bcrypt.hash(password, 5);
-        const user = await User.create({ email, password: hashPassword });
+        const user = await User.create({ email, name, password: hashPassword });
 
         res.status(200).json({
             success: true,
--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
     email: { type: String, unique: true, required: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    name: { type: String, required: true }
 }, { versionKey: false });
 
 module.exports = model('User', userSchema);
