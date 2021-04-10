f92f9921f109c5197ea230b9f743ce11d8ac93f9
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-04-10 13:24:37 +0300

    create comment model and update post model

--- models/Comment.js
+++ models/Comment.js
@@ -1,13 +1,12 @@
 const { Schema, model } = require('mongoose');
 
-const postSchema = new Schema({
+const commentSchema = new Schema({
     userId: { type: Schema.Types.ObjectId, required: true },
-    title: { type: String, required: true },
+    postId: { type: Schema.Types.ObjectId, required: true },
     body: { type: String, required: true },
-    comments: { type: Array, default: [] },
 
     createdAt: { type: Date, default: new Date() },
     updatedAt: { type: Date, default: new Date() }
 }, { versionKey: false });
 
-module.exports = model('Post', postSchema);
+module.exports = model('Comment', commentSchema);
