23ef34b678f9fedc32acd2deb9adc73ea1519267
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-04-10 13:07:50 +0300

    create Post model

---
+++ models/Post.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require('mongoose');
+
+const postSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, required: true },
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+
+    createdAt: { type: Date, default: new Date() },
+    updatedAt: { type: Date, default: new Date() }
+}, { versionKey: false });
+
+module.exports = model('Post', postSchema);
