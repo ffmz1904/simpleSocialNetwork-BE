ac869cc6ee7158ded5fcb96ff889b24304eb210b
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-04-11 14:29:24 +0300

    update getAllPost method

--- controllers/postController/getAllPosts.js
+++ controllers/postController/getAllPosts.js
@@ -1,30 +1,38 @@
 const ApiError = require('../../errors/ApiError');
+const { Types } = require('mongoose');
 const Post = require('../../models/Post');
 
 module.exports = async (req, res, next) => {
+    const {id} = req.params;
+    const aggregationQuery = [];
+
+    if (id) {
+        aggregationQuery.unshift({ $match: { userId: Types.ObjectId(id) } });
+    }
+
+    aggregationQuery.push({ $unwind: {
+            path: '$comments',
+            preserveNullAndEmptyArrays: true
+        }},
+        { $lookup: {
+                from: 'comments',
+                localField: 'comments',
+                foreignField: '_id',
+                as: 'commentData'
+            }},
+        { $group: {
+                "_id": "$_id",
+                "title": { "$first": "$title" },
+                "body": { "$first": "$body" },
+                "userId": { "$first": "$userId" },
+                "comments": { "$push": "$commentData" },
+                "createdAt": { "$first": "$createdAt" },
+                "updatedAt": { "$first": "$updatedAt" },
+            }},
+        { $sort: {createdAt: -1} });
+
     try {
-        const posts = await Post.aggregate([
-            { $unwind: {
-                path: '$comments',
-                preserveNullAndEmptyArrays: true
-            }},
-            { $lookup: {
-                    from: 'comments',
-                    localField: 'comments',
-                    foreignField: '_id',
-                    as: 'commentData'
-                }},
-            { $group: {
-                    "_id": "$_id",
-                    "title": { "$first": "$title" },
-                    "body": { "$first": "$body" },
-                    "userId": { "$first": "$userId" },
-                    "comments": { "$push": "$commentData" },
-                    "createdAt": { "$first": "$createdAt" },
-                    "updatedAt": { "$first": "$updatedAt" },
-            }},
-            { $sort: {createdAt: -1} },
-        ]);
+        const posts = await Post.aggregate(aggregationQuery);
 
         res.status(200).json({
             success: true,
