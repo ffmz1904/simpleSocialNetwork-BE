9b04667e8917d8c3009c44ce5fa4dbee4cc94498
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-04-11 13:04:32 +0300

    add check user method

---
+++ controllers/userController/check.js
@@ -0,0 +1,19 @@
+const ApiError = require('../../errors/ApiError');
+const generateJWT = require('../../helpers/generateJWT');
+const User = require('../../models/User');
+
+module.exports = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.user._id);
+
+        if (!user) {
+            return next(ApiError.notFound('User not found!'));
+        }
+
+        const token = generateJWT(req.user._id, req.user.email);
+
+        res.status(200).json({ token, user });
+    } catch (e) {
+        next(ApiError.internal(e.message));
+    }
+};
--- controllers/userController/index.js
+++ controllers/userController/index.js
@@ -1,11 +1,13 @@
 const registration = require('./registration');
 const login = require('./login');
+const check = require('./check');
 const create_subscribe_request = require('./create_subscribe_request');
 const confirm_subscribe_request = require('./confirm_subscribe_request');
 
 module.exports = {
     registration,
     login,
+    check,
     create_subscribe_request,
     confirm_subscribe_request
 };
