9330967e70481e717740683d87528e338740b113
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-04-12 13:46:27 +0300

    change returned object in createComment

--- controllers/commentController/createComment.js
+++ controllers/commentController/createComment.js
@@ -1,4 +1,5 @@
 const ApiError = require('../../errors/ApiError');
+const { Types } = require('mongoose');
 const Comment = require('../../models/Comment');
 const Post = require('../../models/Post');
 
@@ -17,13 +18,27 @@ module.exports = async (req, res, next) => {
             return next(ApiError.notFound(`Post with id = ${postId} not found!`));
         }
 
-        const comment = await Comment.create({ userId, postId, body });
-        post.comments.push(comment._id);
+        const createdComment = await Comment.create({ userId, postId, body });
+        post.comments.push(createdComment._id);
         await post.save();
+
+        const comment = await Comment.aggregate([
+            { $match: { _id: Types.ObjectId(createdComment._id)}},
+            { $lookup: {
+                    from: 'users',
+                    localField: 'userId',
+                    foreignField: '_id',
+                    as: 'userData'
+                }},
+            { $unwind: {
+                    "path": "$userData",
+                    "preserveNullAndEmptyArrays": true
+            }},
+        ]);
 
         res.status(200).json({
            success: true,
-           comment
+           comment: comment.shift()
         });
     } catch (e) {
         next(ApiError.internal(e.message));
