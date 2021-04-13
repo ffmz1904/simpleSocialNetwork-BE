5cff4fe9243c785bf2643896b28e58ff0343ec6b
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-04-13 10:15:40 +0300

    add search by name query

--- controllers/userController/getAllUsers.js
+++ controllers/userController/getAllUsers.js
@@ -2,14 +2,23 @@ const ApiError = require('../../errors/ApiError');
 const User = require('../../models/User');
 
 module.exports = async (req, res, next) => {
+    const { name } = req.query;
+    let users;
+
     try {
-        const users = await User.find();
+        if (name) {
+            const nameFilter = new RegExp(name, 'i');
+            users = await User.find({name: nameFilter});
+        } else {
+            users = await User.find();
+        }
 
         res.status(200).json({
            success: true,
             users
         });
     } catch (e) {
+        console.log('Error ', e.message);
         next(ApiError.internal(e.message));
     }
 };
--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const start = async () => {
         });
 
         app.listen(PORT, () => {
-            console.log(`Serer has been started on localhost:${PORT}`);
+            console.log(`Server has been started on localhost:${PORT}`);
         });
     } catch (e) {
         console.log(e.message);
