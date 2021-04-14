08c390c033b03393d754582e5ddf2a7326100da2
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-04-14 13:51:31 +0300

    add autoRemove old image when user update img

--- controllers/userController/updateUser.js
+++ controllers/userController/updateUser.js
@@ -3,22 +3,24 @@ const User = require('../../models/User');
 const ApiError = require('../../errors/ApiError');
 const path = require('path');
 const uuid = require('uuid');
+const fs = require('fs');
 
 module.exports = async (req, res, next) => {
     const { _id: userId } = req.user;
-    const { update } = req.body;
-    const { img } = req.files;
+    const { ...update } = req.body;
+    let img = req.files ? req.files.img : null;
     let fileName = uuid.v4() + '.jpeg';
 
-    if (img) {
-        await img.mv(path.resolve(__dirname, '..', '..', 'static', 'user_images', fileName));
-    }
-
-    if (!update) {
+    if (!update && img) {
         return next(ApiError.badRequest('Can\'t update user without update params'));
     }
 
     try {
+        if (img) {
+            await img.mv(path.resolve(__dirname, '..', '..', 'static', 'user_images', fileName));
+            update.img = 'user_images/' + fileName;
+        }
+
         if (update.password) {
             const oldPassword = await User.findOne({ _id: userId }, { password: 1, _id: 0});
             const comparePassword = await bcrypt.compare(update.oldPassword, oldPassword.password);
@@ -28,6 +30,13 @@ module.exports = async (req, res, next) => {
             }
             delete update.oldPassword;
             update.password = await bcrypt.hash(update.password, 5);
+        }
+
+        if (img) {
+            const oldImg = await User.findOne({ _id: userId }, { img: 1, _id: 0});
+            if (oldImg.img !== process.env.DEFAULT_USER_IMG) {
+                fs.unlink(path.resolve(__dirname, '..', '..', 'static', oldImg.img), (err) => {});
+            }
         }
 
         await User.findOneAndUpdate({ _id: userId }, update);
