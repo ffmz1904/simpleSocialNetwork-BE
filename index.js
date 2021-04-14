a679f147d794ff102b59f9e93a47266a555a2979
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-04-14 17:46:25 +0300

    fix

--- index.js
+++ index.js
@@ -7,15 +7,21 @@ const router = require('./routes');
 const errorHandler = require('./middlewares/ErrorHandlingMiddleware');
 const fileUpload = require('express-fileupload');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 app.use(fileUpload({}));
 app.use('/api', router);
+app.use('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+});
+
 app.use(errorHandler);
+
 
 const start = async () => {
     try {
